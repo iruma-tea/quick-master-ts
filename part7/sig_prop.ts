1e1552d4b97e659b01e7d875400840c67ce23684
shusuke-iruma-tea <shusuke.pana@gmail.com>
2023-08-30 21:19:53 +0900

    メソッドシグネチャまで学習完了

---
+++ part7/sig_prop.ts
@@ -0,0 +1,13 @@
+{
+    interface Person {
+        readonly name: string;
+        age?: number;
+    };
+
+    let p: Person = {
+        name: '田中太郎',
+    };
+
+    // p.name = '鈴木三郎'; // エラー
+
+}
